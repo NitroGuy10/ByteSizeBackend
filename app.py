cedecc9cb0b3e4733f19cdb8fd1df13b7b0ae66b
NitroGuy10 <naviyer10@gmail.com>
2022-10-16 03:18:48 -0500

    MVP backend done!!!

--- app.py
+++ app.py
@@ -30,27 +30,14 @@ s3_client = boto3.client("s3", config=s3_config,
 def hello_world():
     return send_file("upload.html")
 
-@app.route("/<video_id>/ascii")
-def get_video_ascii(video_id):
-    return "<p>TODO return the ascii text file</p>"
-
-@app.route("/<video_id>/original")
-def get_video_original(video_id):
-    return "<p>TODO return the original (mp4) video file</p>"
-
-@app.route("/<video_id>/uncompressed")
-def get_video_uncompressed(video_id):
-    return "<p>TODO return the uncompressed video file</p>"
-
-@app.route("/process_video", methods=["POST"])
-def upload_and_process_video():
+@app.route("/video_to_text", methods=["POST"])
+def video_to_text():
     if request.method == "POST":
         # Read the request
         request_data = request.get_json()
         bucket_location = request_data["bucketVideoKey"]
-        video_uuid = uuid.uuid4()
-        bucket_text_location = f"{os.environ['bucketTextDirName']}/{video_uuid}.txt"
-        bucket_reconstructed_video_location = f"{os.environ['bucketVideoDirName']}/{video_uuid}.mp4"
+        text_uuid = uuid.uuid4()
+        bucket_text_location = f"{os.environ['bucketTextDirName']}/{text_uuid}.txt"
 
         # Do the thing
         downloaded_file_name = "downloaded.mp4"
@@ -62,11 +49,27 @@ def upload_and_process_video():
         exported_text_file_name = "res.txt"
         s3_client.upload_file(exported_text_file_name, os.environ["bucketName"], bucket_text_location)
 
-        # Now prepare for ML on the text
-        exported_ml_text_file_name = "figure/res2.txt"
-        if os.path.exists(exported_ml_text_file_name):
-            os.remove(exported_ml_text_file_name)
-        shutil.copy(exported_text_file_name, exported_ml_text_file_name)
+        # Done, now send a response
+        return_dict = {
+            "bucketTextLocation": bucket_text_location
+        }
+        print(return_dict)
+        return return_dict
+    else:
+        abort(400)
+
+@app.route("/text_to_video", methods=["POST"])
+def text_to_video():
+    if request.method == "POST":
+        # Read the request
+        request_data = request.get_json()
+        bucket_location = request_data["bucketTextKey"]
+        video_uuid = uuid.uuid4()
+        bucket_reconstructed_video_location = f"{os.environ['bucketVideoDirName']}/{video_uuid}.mp4"
+
+        # Prepare for ML on the text
+        downloaded_file_name = "figure/res2.txt"
+        s3_client.download_file(os.environ["bucketName"], bucket_location, downloaded_file_name)        
 
         # Do the ML
         inference.real_main("res2")
@@ -75,17 +78,14 @@ def upload_and_process_video():
         reconstructed_video_path = "figure/videos/video_res2.mp4"
         s3_client.upload_file(reconstructed_video_path, os.environ["bucketName"], bucket_reconstructed_video_location)
 
-
         # Done, now send a response
         return_dict = {
-            "bucketTextLocation": bucket_text_location,
             "bucketReconstructedVideoLocation": bucket_reconstructed_video_location
         }
         print(return_dict)
         return return_dict
     else:
         abort(400)
-
 
 
 #s3_client.upload_file("rickroll.mp4", os.environ["bucketName"], "videos/rickroll.mp4")
