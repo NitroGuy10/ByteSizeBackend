5866fcd944f2bf57eb47bc7d59241d5679735342
Abhishek <abhimore74@gmail.com>
2022-10-15 23:37:43 -0500

    added file creation

--- compress.py
+++ compress.py
@@ -5,17 +5,19 @@ import math
 
 height = 480
 width = 640
-chars = [".", ",", ":", ";", "+", "*", "?", "%", "S", "#", "@"]
-#chars = [' ', '`','^','"',',',':',';','I','l','!','i','~','+','_','-','?',']',
-#        '[','}','{','1',')','(','|','/','t','f','j','r','x','n','u','v','c','z',
- #       'X','Y','U','J','C','L','Q','0','O','Z','m','w','q','p','d','b','k','h',
- #       'a','o','*','#','M','W','&','8','%','B','@','$']
+#chars = [".", ",", ":", ";", "+", "*", "?", "%", "S", "#", "@"]
+chars = [' ', '`','^','"',',',':',';','I','l','!','i','~','+','_','-','?',']',
+        '[','}','{','1',')','(','|','/','t','f','j','r','x','n','u','v','c','z',
+        'X','Y','U','J','C','L','Q','0','O','Z','m','w','q','p','d','b','k','h',
+        'a','o','*','#','M','W','&','8','%','B','@','$']
 def resizeImg(img, newWidth):
     newMat = cv2.Mat(np.zeros((1,1)))
     width = len(img[0]);
     height = len(img);
     ratio = height / width;
-    newHeight = math.floor(newWidth * ratio * 0.75)
+
+    newHeight=60
+    #newHeight = math.floor(newWidth * ratio * 0.75)
 
     newMat = cv2.resize(img, (newWidth, newHeight))
     return newMat;
@@ -31,8 +33,8 @@ def asciify(img):
     for cols in range(len(img[0])):
       #pixel = data[rows * len(img[0]) + cols]
       pixel = data[rows][cols]
-      #newChar = chars[math.floor(pixel / 4)]
-      newChar = chars[math.floor(pixel / 25)]
+      newChar = chars[math.floor(pixel / 4)]
+      #newChar = chars[math.floor(pixel / 25)]
       characterLine += newChar
 
     characterLine += "\n"
@@ -50,6 +52,7 @@ def printMat(img):
       print(elem)
 
 def processVideo(filename):
+  res = open('res.txt', 'w')
   #src = cv2.Mat(480, 640, cv2.CV_8UC4)
   src = cv2.Mat(np.zeros((480, 640, 4), dtype='uint8'))
   cap = cv2.VideoCapture(filename)
@@ -59,8 +62,9 @@ def processVideo(filename):
     frame = resizeImg(src, 120);
     frame = grayify(frame);
     characters = asciify(frame);
-    #TODO: Put characters into a text file and do whatever with it
+    res.write(characters)
     _, src = cap.read(src)
+    time.sleep(0.015)
 
 
 processVideo("testvid.mp4")
